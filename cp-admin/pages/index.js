c6809b4d82ba08620e1b4934c04325e300ee04b0
jiji15899-arch <jiji15899@gmail.com>
2026-04-14 18:36:31 +0900

    Create index.js

---
+++ cp-admin/pages/index.js
@@ -0,0 +1,43 @@
+/**
+ * CloudPress Admin - Remaining Page Handlers
+ * Stubs + full implementations for:
+ *  - media.js         (replaces wp-admin/upload.php)
+ *  - pages.js         (replaces wp-admin/edit.php?post_type=page)
+ *  - comments.js      (replaces wp-admin/edit-comments.php)
+ *  - themes.js        (replaces wp-admin/themes.php)
+ *  - plugins.js       (replaces wp-admin/plugins.php)
+ *  - users.js         (replaces wp-admin/users.php)
+ *  - user-edit.js     (replaces wp-admin/user-edit.php)
+ *  - profile.js       (replaces wp-admin/profile.php)
+ *  - options.js       (replaces wp-admin/options.php)
+ *  - options-writing.js  (wp-admin/options-writing.php)
+ *  - options-reading.js  (wp-admin/options-reading.php)
+ *  - options-discussion.js (wp-admin/options-discussion.php)
+ *  - options-media.js (wp-admin/options-media.php)
+ *  - options-permalink.js (wp-admin/options-permalink.php)
+ *  - import.js        (wp-admin/import.php)
+ *  - export.js        (wp-admin/export.php)
+ *  - tools.js         (wp-admin/tools.php)
+ *  - upgrade.js       (wp-admin/update-core.php)
+ *
+ * @package CloudPress
+ */
+
+export * from './media.js';
+export * from './pages.js';
+export * from './comments.js';
+export * from './themes.js';
+export * from './plugins.js';
+export * from './users.js';
+export * from './user-edit.js';
+export * from './profile.js';
+export * from './options.js';
+export * from './options-writing.js';
+export * from './options-reading.js';
+export * from './options-discussion.js';
+export * from './options-media.js';
+export * from './options-permalink.js';
+export * from './import.js';
+export * from './export.js';
+export * from './tools.js';
+export * from './upgrade.js';
